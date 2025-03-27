f2b18e802b62c28f0b2049fd447f0a905cd99151
Jeremie Carrel <146066884+Hira-shi@users.noreply.github.com>
2025-03-27 10:54:17 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module ch.gol.gameoflife {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens ch.gol.gameoflife to javafx.fxml;
+    exports ch.gol.gameoflife;
+}
